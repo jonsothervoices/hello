1f9b6b8cc883fb512391d8f1c602ddf218b7214e
Jonathan Miller <jonsothervoices@gmail.com>
2020-11-23 16:47:35 -0500

    added cleaner 1.5 oneAway

--- stringexercises/oneaway.go
+++ stringexercises/oneaway.go
@@ -2,42 +2,39 @@ package stringexercises
 
 import "math"
 
-//three types of edits: insert, remove, or replace a character.
-//Given two strings, writa a function to check if they are one edit away.
-//oneAway is O(n)
+//oneAway has O(n)
 func oneAway(a, b string) bool {
-	//early exit opportunity
-	//length difference greater than 2 requires more than 1 edit
 	if math.Abs(float64(len(b)-len(a))) > 1 {
 		return false
 	}
+	if len(a) == len(b) {
+		return replaceCheck(a, b)
+	}
+	if len(a)+1 == len(b) {
+		return insertCheck(a, b)
+	}
+	return insertCheck(b, a)
+}
 
-	for i, char1 := range a {
-		//check length
-		if len(b) <= i {
-			return b == a[:i]
+func replaceCheck(a, b string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return len(a) == i+1 || a[i+1:] == b[i+1:]
 		}
+	}
+	return true
+}
 
-		//found an edit
-		if char1 != rune(b[i]) {
-			//are we at the end of either a or b?
-			if len(b) <= i+1 || len(a) <= i+1 {
-				//only one edit, on last char
-				return len(b) == len(a)
-			}
-			//insert case
-			if a[i:] == b[i+1:] {
-				return true
-			}
-			//remove case
-			if a[i+1:] == b[i:] {
-				return true
-			}
-			//replace case
-			if a[i+1:] == b[i+1:] {
-				return true
-			}
-			return false
+func insertCheck(a, b string) bool {
+	if len(a)+1 != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return a[i:] == b[i+1:]
 		}
 	}
 	return true
