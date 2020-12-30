a9c4f570d23091b051565130261da7803bbcec1a
Jonathan Miller <jonsothervoices@gmail.com>
2020-12-30 15:30:37 -0500

    added 5.4, 5.5, 5.6, and started 5.7

--- bits/bit_test.go
+++ bits/bit_test.go
@@ -1,6 +1,7 @@
 package bits
 
 import (
+	"math"
 	"testing"
 )
 
@@ -186,6 +187,110 @@ func TestRSum(t *testing.T) {
 	}
 	for i, datest := range tests {
 		actual := rSum(datest.s, datest.i)
+		if datest.expected != actual {
+			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
+		}
+	}
+}
+
+func TestNextLNum(t *testing.T) {
+	var tests = []struct {
+		n        int64
+		expected int64
+	}{
+		{9, 10},    //1001, 1010
+		{59, 61},   //00111011, 00111101
+		{315, 317}, //0100111011, 0100111101
+		{64, 128},  //01000000, 10000000
+		{63, 95},   //00111111, 01011111
+		{1, 2},     //00000001, 00000010
+		{0, 0},     //00000000, 00000000
+		{4294967295, 6442450943},
+		{math.MaxInt64, 0},
+		{math.MaxInt64 - 1, 0},
+	}
+	for i, datest := range tests {
+		actual, err := nextLNum(datest.n)
+		if err != nil && datest.expected != 0 {
+			t.Errorf("%v: actual %v, expected %v", i, actual, err)
+			t.FailNow()
+		}
+		if err == nil && datest.expected == 0 {
+			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
+			t.FailNow()
+		}
+		if datest.expected != actual {
+			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
+		}
+	}
+}
+
+func TestNextSNum(t *testing.T) {
+	var tests = []struct {
+		n        int64
+		expected int64
+	}{
+		{9, 5},     //1001, 0101
+		{59, 55},   //00111011, 00110111
+		{315, 311}, //0100111011, 0100110111
+		{64, 32},   //01000000, 00100000
+		{63, 0},    //00111111, 00111111
+		{1, 0},     //00000001, 00000001
+		{0, 0},     //00000000, 00000000
+		{2, 1},
+		{4294967295, 0}, //11111111111111111111111111111111
+		{1022, 1021},
+	}
+	for i, datest := range tests {
+		actual, err := nextSNum(datest.n)
+		if err != nil && datest.expected != 0 {
+			t.Errorf("%v: actual %v, expected %v", i, actual, err)
+			t.FailNow()
+		}
+		if err == nil && datest.expected == 0 {
+			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
+			t.FailNow()
+		}
+		if datest.expected != actual {
+			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
+		}
+	}
+}
+
+func TestConv(t *testing.T) {
+	var tests = []struct {
+		a        int64
+		b        int64
+		expected int
+	}{
+		{9, 5, 2},   //1001, 0101
+		{33, 63, 4}, //100001, 111111
+		{33, 59, 3}, //100001, 111011
+		{12, 12, 0},
+		{1, 0, 1},
+	}
+	for i, datest := range tests {
+		actual := conv(datest.a, datest.b)
+		if datest.expected != actual {
+			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
+		}
+	}
+}
+
+func TestPairSwap(t *testing.T) {
+	var tests = []struct {
+		a        int64
+		expected int64
+	}{
+		{9, 6}, //1001
+		// {33, 18},   //100001
+		// {170, 85},  //10101010, 01010101
+		// {366, 669}, //0101101110, 1010011101
+		// {1, 2},
+		// {2, 1},
+	}
+	for i, datest := range tests {
+		actual := pairSwap(datest.a)
 		if datest.expected != actual {
 			t.Errorf("%v: actual %v, expected %v", i, actual, datest.expected)
 		}
--- bits/bitfunctions.go
+++ bits/bitfunctions.go
@@ -2,6 +2,7 @@ package bits
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -88,5 +89,103 @@ func rSum(s string, n int) int {
 	for i := n + 1; i < len(s) && s[i] != '0'; i++ {
 		ret++
 	}
+	return ret
+}
+
+//5.3 Next Number: Given a positive integer, print the next smallest and the next largest number that have the same number of 1 bits in their binary representation.
+
+//smallest number bigger--O(n) where n is the langth of bin rep of n
+//12:53~~1:30
+func nextLNum(n int64) (int64, error) {
+	//check max
+	if n >= math.MaxInt64-1 {
+		return 0, fmt.Errorf("error: %v out of range", n)
+	}
+	//bin rep
+	nBin := "0" + strconv.FormatInt(n, 2)
+	//find last 01
+	i := strings.LastIndex(nBin, "01")
+	//no 01?
+	if i == -1 {
+		return 0, fmt.Errorf("error: %v has all zeros: %v", n, nBin)
+	}
+	//flip last 01
+	nBin = fmt.Sprintf("%v10%v", nBin[:i], nBin[i+2:])
+	return strconv.ParseInt(nBin, 2, 64)
+}
+
+//largest number smaller
+func nextSNum(n int64) (int64, error) {
+	//check min(2)
+	if n < 2 {
+		return 0, fmt.Errorf("error: %v out of range", n)
+	}
+	//bin rep
+	nBin := strconv.FormatInt(n, 2)
+	//find last 10
+	i := strings.LastIndex(nBin, "10")
+	//no 10?
+	if i == -1 {
+		return 0, fmt.Errorf("error: %v has no nextSNum: %v", n, nBin)
+	}
+	//flip last 10
+	nBin = fmt.Sprintf("%v01%v", nBin[:i], nBin[i+2:])
+	return strconv.ParseInt(nBin, 2, 64)
+}
+
+//5.5: Debugger:
+//((n&(n-1))==0)
+//n==50
+//n-1==49
+//110010 &
+//110001 == 0 ?
+//110010 &
+//001101 == 0
+//when is n-1 the compliment of n?
+//64==1000000
+//63==0111111
+//((n&(n-1))==0) iff n%2==0 ie. n is a power of 2
+//this is a power of 2 check
+
+//5.6: Conversion: Write a function to determine the number of bits you would need to flip to convert integer A to integer B (assume positive A and B).
+
+//2:13~~2:21
+func conv(a, b int64) int {
+	s := strconv.FormatInt(a^b, 2)
+	return strings.Count(s, "1")
+}
+
+//5.7:Pairwise swap: Write a function to swap odd and even bits in an integer with as few instructions as possible
+//2:28
+func pairSwap(a int64) int64 {
+	//maskL:shift a left 1
+	//maskR:shift a right 1
+	s := strconv.FormatInt(int64(a), 2)
+	fmt.Printf("s is %v\n", s)
+	len := len(s) - 1
+	maskL := fmt.Sprintf("%v0", s)
+	maskR := fmt.Sprintf("0%v", s[:len])
+	fmt.Printf("maskL is %v\n", maskL)
+	fmt.Printf("maskR is %v\n", maskR)
+	//make new slice s with 0s length binrep of a
+	for i := range s {
+		//range check
+		if i == len {
+			s = fmt.Sprintf("%v%v", s[:i], maskL[i])
+			fmt.Printf("i==len: s is now %v\n", s)
+			continue
+		}
+		//if even, assign s[i] to maskL[i]
+		if i%2 == 0 {
+			s = fmt.Sprintf("%v%v%v", s[:i], maskL[i], s[i+1:])
+			fmt.Printf("i is even: s is now %v\n", s)
+			continue
+		}
+		//if index is odd, assign new[i] to maskR[i]
+		s = fmt.Sprintf("%v%v%v", s[:i], maskR[i], s[i+1:])
+		fmt.Printf("i is odd: s is now %v\n", s)
+	}
+	ret, _ := strconv.ParseInt(s, 2, 64)
+	fmt.Printf("returning %v\n", ret)
 	return ret
 }
